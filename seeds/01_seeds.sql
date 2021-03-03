20437ea8e527f60af9d238ac03f751a7a72baeb2
Hosam  Dahrooge <Hosam_firas@hotmail.com>
2021-03-03 14:53:12 -0700

    finalize data entry

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,3 +1,4 @@
+-- I saw the next exercise. I won't spend more time on data entry. Thank you.
 DELETE FROM users;
 
 INSERT INTO users (name, email, password)
@@ -9,7 +10,3 @@ VALUES ('Eva Stanley', 'sebastianguer@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvme
 ('Etta West', 'charlielevy@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
 ('Margaret Wong', 'makaylaweiss@icloud.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
 ('Leroy Hart', 'jaycereynolds@inbox.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
-
-
-
-
